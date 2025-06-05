924a57c2427bc5fd24fe42091dd377ac0073c20c
Roby Karti S <robyfull.dev@gmail.com>
2025-06-05 17:11:23 +0700

    Push

--- resources/js/pages/Products/Index.tsx
+++ resources/js/pages/Products/Index.tsx
@@ -10,7 +10,7 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-export default function Dashboard() {
+export  default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Products" />
